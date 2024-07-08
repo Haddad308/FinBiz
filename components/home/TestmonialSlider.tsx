3dc55e078dd928b56d8331087e0e231bb5767a43
Haddad308 <melhaddad308@gmail.com>
2024-07-09 02:08:50 +0300

    fix

--- components/home/TestmonialSlider.tsx
+++ components/home/TestmonialSlider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { use, useEffect } from "react";
+import React, { useEffect } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
