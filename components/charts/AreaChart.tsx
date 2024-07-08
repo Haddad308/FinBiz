009b5743b1d51f73c3c66ecc3fc0c10d66891f94
Haddad308 <melhaddad308@gmail.com>
2024-07-09 01:09:37 +0300

    fix chart

--- components/charts/AreaChart.tsx
+++ components/charts/AreaChart.tsx
@@ -1,63 +1,88 @@
 "use client";
 
-import { useState } from "react";
-import ReactApexChart from "react-apexcharts";
-import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
-const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
+import { useState, useEffect } from "react";
+import { ApexOptions } from "apexcharts";
+
+const ReactApexChart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const ApexChart: React.FC = () => {
-  const [series, setSeries] = useState([
-    {
-      name: "series1",
-      data: [31, 40, 28, 51, 42, 109, 100]
-    },
-    {
-      name: "series2",
-      data: [11, 32, 45, 32, 34, 52, 41]
-    }
-  ]);
+  const [mounted, setMounted] = useState(false);
+  const [series, setSeries] = useState<{ name: string; data: number[] }[]>([]);
+  const [options, setOptions] = useState<ApexOptions>({});
 
-  const [options, _] = useState<ApexOptions>({
-    chart: {
-      height: 350,
-      type: "area"
-    },
-    dataLabels: {
-      enabled: false
-    },
-    stroke: {
-      curve: "smooth"
-    },
-    xaxis: {
-      type: "datetime",
-      categories: [
-        "2018-09-19T00:00:00.000Z",
-        "2018-09-19T01:30:00.000Z",
-        "2018-09-19T02:30:00.000Z",
-        "2018-09-19T03:30:00.000Z",
-        "2018-09-19T04:30:00.000Z",
-        "2018-09-19T05:30:00.000Z",
-        "2018-09-19T06:30:00.000Z"
-      ]
-    },
-    tooltip: {
-      x: {
-        format: "dd/MM/yy HH:mm"
+  useEffect(() => {
+    setMounted(true);
+
+    const seriesData = {
+      monthDataSeries1: {
+        prices: [31, 40, 28, 51, 42, 109, 100],
+        dates: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z"
+        ]
       }
-    }
-  });
+    };
+
+    setSeries([
+      {
+        name: "STOCK ABC",
+        data: seriesData.monthDataSeries1.prices
+      }
+    ]);
+
+    setOptions({
+      chart: {
+        type: "area",
+        height: 350,
+        zoom: {
+          enabled: false
+        }
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: "straight"
+      },
+
+      subtitle: {
+        text: "Price Movements",
+        align: "left"
+      },
+      labels: seriesData.monthDataSeries1.dates,
+      xaxis: {
+        type: "datetime"
+      },
+      yaxis: {
+        opposite: true
+      },
+      legend: {
+        horizontalAlign: "left"
+      }
+    });
+  }, []);
 
   return (
     <div>
-      <div id="chart">
-        <ReactApexChart
-          options={options}
-          series={series}
-          type="area"
-          height={350}
-        />
-      </div>
+      {mounted ? (
+        <div id="chart">
+          <ReactApexChart
+            options={options}
+            series={series}
+            type="area"
+            height={350}
+            width={"100%"}
+          />
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
       <div id="html-dist"></div>
     </div>
   );
