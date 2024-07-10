6ceba156029340e8093e0baf51077260b76feaeb
Haddad308 <melhaddad308@gmail.com>
2024-07-10 04:34:38 +0300

    check solu

--- app/comments/page.tsx
+++ app/comments/page.tsx
@@ -4,7 +4,7 @@ import Comment from "@/components/Comment";
 import CommentSk from "@/components/CommentSk";
 import { addComment, fetchComments } from "@/handlers";
 import { Button, Pagination, Textarea } from "@nextui-org/react";
-import { Key, useEffect, useState } from "react";
+import { Key, Suspense, useEffect, useState } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { useRouter, useSearchParams } from "next/navigation";
@@ -62,69 +62,71 @@ export default function Comments() {
   };
 
   return (
-    <form onSubmit={formik.handleSubmit}>
-      <div className="flex  items-center justify-center">
-        <main className="my-20 flex w-5/6 flex-col items-center justify-center md:w-1/3">
-          <h1 className="mb-5 self-start text-2xl font-bold">Add your comment</h1>
-          <div className="w-full">
-            <Textarea
-              label="Comment"
-              name="body"
-              variant="bordered"
-              placeholder="Enter your comment here..."
-              disableAnimation
-              disableAutosize
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.body.trim()}
-              classNames={{
-                base: "w-full",
-                input: "resize-y min-h-[40px]"
-              }}
-            />
-            {formik.touched.body && !!formik.errors.body && (
-              <span className="ms-1 text-sm text-red-500 ">{formik.errors.body}</span>
-            )}
-          </div>
-          <div className="mt-5 self-start">
-            <Button
-              color="primary"
-              type="submit"
-            >
-              Post
-            </Button>
-          </div>
-          <div className="mt-5 h-[1px] w-full bg-gray-300" />
-          <h1 className="mb-5 mt-5 self-start text-2xl font-bold">Comments</h1>
-          <div className="mb-5 flex w-full flex-col gap-4">
-            {isLoading ? (
-              <>
-                <CommentSk />
-                <CommentSk />
-                <CommentSk />
-              </>
-            ) : (
-              comments.map(
-                (comment: { id: Key | null | undefined; user: { fullName: string }; body: string }, idx: number) => (
-                  <Comment
-                    id={idx * page}
-                    key={comment.id}
-                    userName={comment.user.fullName}
-                    commentBody={comment.body}
-                  />
+    <Suspense fallback={null}>
+      <form onSubmit={formik.handleSubmit}>
+        <div className="flex  items-center justify-center">
+          <main className="my-20 flex w-5/6 flex-col items-center justify-center md:w-1/3">
+            <h1 className="mb-5 self-start text-2xl font-bold">Add your comment</h1>
+            <div className="w-full">
+              <Textarea
+                label="Comment"
+                name="body"
+                variant="bordered"
+                placeholder="Enter your comment here..."
+                disableAnimation
+                disableAutosize
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.body.trim()}
+                classNames={{
+                  base: "w-full",
+                  input: "resize-y min-h-[40px]"
+                }}
+              />
+              {formik.touched.body && !!formik.errors.body && (
+                <span className="ms-1 text-sm text-red-500 ">{formik.errors.body}</span>
+              )}
+            </div>
+            <div className="mt-5 self-start">
+              <Button
+                color="primary"
+                type="submit"
+              >
+                Post
+              </Button>
+            </div>
+            <div className="mt-5 h-[1px] w-full bg-gray-300" />
+            <h1 className="mb-5 mt-5 self-start text-2xl font-bold">Comments</h1>
+            <div className="mb-5 flex w-full flex-col gap-4">
+              {isLoading ? (
+                <>
+                  <CommentSk />
+                  <CommentSk />
+                  <CommentSk />
+                </>
+              ) : (
+                comments.map(
+                  (comment: { id: Key | null | undefined; user: { fullName: string }; body: string }, idx: number) => (
+                    <Comment
+                      id={idx * page}
+                      key={comment.id}
+                      userName={comment.user.fullName}
+                      commentBody={comment.body}
+                    />
+                  )
                 )
-              )
-            )}
-          </div>
-          <Pagination
-            onChange={(page: number) => handlePageChange(page)}
-            isCompact
-            showControls
-            total={Math.ceil(total / ITEMS)}
-            initialPage={urlPage}
-          />
-        </main>
-      </div>
-    </form>
+              )}
+            </div>
+            <Pagination
+              onChange={(page: number) => handlePageChange(page)}
+              isCompact
+              showControls
+              total={Math.ceil(total / ITEMS)}
+              initialPage={urlPage}
+            />
+          </main>
+        </div>
+      </form>
+    </Suspense>
   );
 }
