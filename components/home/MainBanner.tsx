11e775a3a5aa2621dccbaa1f27a267f13486f570
Haddad308 <melhaddad308@gmail.com>
2024-07-12 02:43:31 +0300

    ss

--- components/home/MainBanner.tsx
+++ components/home/MainBanner.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import AnalyticsCard from "./AnalyticsCard";
 import { Button } from "@nextui-org/react";
-import ApexChart from "../charts/AreaChart";
 import Image from "next/image";
 import { useTheme } from "next-themes";
 
